24e74cc0823ac2db76e33a086b282a144759e718
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2026-09-18 17:35:48 +0200

    change

--- drbg.py
+++ drbg.py
@@ -1,17 +1,4 @@
-"""
-drbg.py
-=======
-HMAC-DRBG minimal (inspiré de NIST SP 800-90A, non certifié / usage
-pédagogique) : transforme une graine unique (la sortie de
-HybridEntropyExtractor) en un flux pseudo-aléatoire arbitrairement long.
 
-Utilisé par key_generator.py pour la recherche des nombres premiers RSA,
-car la génération de clé RSA "standard" (cryptography/OpenSSL) ne permet
-pas d'injecter directement notre propre entropie hybride : OpenSSL pioche
-dans son propre RNG interne. En pilotant nous-mêmes la recherche de
-premiers avec ce DRBG, la totalité de la clé RSA dérive bien de la
-graine hybride quantique+OS.
-"""
 import hmac
 import hashlib
 
@@ -37,5 +24,5 @@ class HmacDrbg:
         while len(out) < n_bytes:
             self._v = self._hmac(self._k, self._v)
             out += self._v
-        self._update()  # backtracking resistance
+        self._update() 
         return out[:n_bytes]
